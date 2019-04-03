8906591f8a841c8e5e8e284484b1cf798be2325f
Brendan Burkhart <blackravenbrendan@gmail.com>
2019-04-02 19:44:56 -0700

    Add average to Ring

--- src/main/java/frc/robot/utils/Ring.java
+++ src/main/java/frc/robot/utils/Ring.java
@@ -8,6 +8,9 @@ public class Ring {
 
     private final int size;
 
+    private double total = 0.0;
+    private double average = 0.0;
+
     public Ring(int size) {
         if (size < 1) {
             throw new IllegalArgumentException("Size of ring must be positive");
@@ -18,15 +21,29 @@ public class Ring {
     }
 
     public void put(double value) {
+        total -= storage[writePos];
+
         storage[writePos] = value;
 
         writePos = (writePos + 1) % size;
+
+        total += value;
+
+        average = total / size;
     }
 
     public double get() {
         double value = storage[readPos];
+        storage[readPos] = 0.0;
+
+        total -= value;
+        average = total / size;
 
         readPos = (readPos + 1) % size;
         return value;
+    }
+
+    public double average() {
+        return average;
     }
 }
